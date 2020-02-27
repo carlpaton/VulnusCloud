0c4ec391996c305dcdad6a1a35fd3ed82beff887
Carl <carl.paton@gmail.com>
2020-02-27 22:10:42 +1300

    app.UseDeveloperExceptionPage for reasons of debug

--- VulnusCloud/Startup.cs
+++ VulnusCloud/Startup.cs
@@ -127,14 +127,14 @@ namespace VulnusCloud
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //}
 
             app.UseStaticFiles();
             app.UseCookiePolicy();
